ca4aa36282b35a0c06eba25fbd42ba0a17ca5d47
Sadera Nye <saderanye1@gmail.com>
2022-04-24 17:26:28 -0700

    Components Part 2

--- 3 - React/nucampsite react/nucampsite/src/index.js
+++ 3 - React/nucampsite react/nucampsite/src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'typeface-lobster';
+import 'typeface-open-sans';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
