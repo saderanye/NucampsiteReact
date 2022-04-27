43a0eb671bd752a5cb55bd79cea57d084aebba07
Sadera Nye <saderanye1@gmail.com>
2022-04-26 20:26:17 -0700

    Redux Actions and Reducers?

--- 3 - React/nucampsite react/nucampsite/src/components/MainComponent.js
+++ 3 - React/nucampsite react/nucampsite/src/components/MainComponent.js
@@ -8,6 +8,8 @@ import Contact from './ContactComponent';
 import About from './AboutComponent';
 import { Switch, Route, Redirect, withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
+import { addComment } from '../redux/ActionCreators';
+
 
 const mapStateToProps = state => {
     return {
@@ -17,6 +19,11 @@ const mapStateToProps = state => {
         promotions: state.promotions
     }
 }
+
+const mapDispatchToProps = {
+    addComment: (campsiteId, rating, author, text) => (addComment(campsiteId, rating, author, text))
+};
+
 class Main extends Component {
 
     render() {
@@ -33,9 +40,10 @@ class Main extends Component {
         const CampsiteWithId = ({match}) => {
             return (
                 <CampsiteInfo 
-                    campsite={this.props.campsites.filter(campsite => campsite.id === +match.params.campsiteId)[0]}
+                    campsite={this.props.campsites.filter(campsite => campsite.id === +match.params.campsiteId)[0]} 
                     comments={this.props.comments.filter(comment => comment.campsiteId === +match.params.campsiteId)}
-                />
+                    addComment={this.props.addComment}
+                />  
             );
         }; 
 
@@ -56,4 +64,4 @@ class Main extends Component {
     }
 }
 
-export default withRouter(connect(mapStateToProps)(Main));
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(Main));
